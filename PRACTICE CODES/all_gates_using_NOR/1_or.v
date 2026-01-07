0cde882a4008a636bf12a91347c104a2df038264
Naveen J <jikkilinaveen@gmail.com>
2026-01-07 15:41:37 +0530

    Create 1_or.v

---
+++ PRACTICE CODES/all_gates_using_NOR/1_or.v
@@ -0,0 +1,32 @@
+module or_nor_gate(A, B, Q);
+  input A;
+  input B;
+  output Q;
+  
+  wire Q1;
+  
+  nor g1(Q1, A, B);
+  nor g2(Q, Q1);
+endmodule
+
+
+// TESTBENCH
+module or_nor_gate_tb;   
+  reg A, B;
+  wire Q;
+
+  or_nor_gate dut(.A(A), .B(B), .Q(Q));
+
+  initial begin
+  $dumpfile(".vcd");
+  $dumpvars();
+
+  A = 0; B = 0;
+  #10; A = 0; B = 1;
+  #10; A = 1; B = 0;
+  #10; A = 1; B = 1;
+  #20;
+  $display("Time=%0t : Calling $finish",$time);
+  $finish;
+end
+endmodule
